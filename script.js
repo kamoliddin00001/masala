ed847e9040583aaa81b0eaac18ed3e137cb7dd27
misol <xoshm73@gmail.com>
2022-04-04 18:21:53 +0800

    misol

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+let ism = prompt('ismingizni kiriting')
+let yosh = prompt('yoshingizni kiriting')
+let odam = prompt('misolni yechin 20+15')
+let keyin = prompt('misolni yechin 6*9')
+let hello = prompt('misolni yechin 9*10')
+let kettik = prompt('misolni yechin 30*5');
+let kim = prompt('misolni yechin 100-15');
+let world = prompt('misolni yechin 5*6');
+let school = prompt('misolni yechin 2*2');
+let index = prompt('misolni yechin 30*2');
+alert ("javobni ko'rish uchun ko'nsolga kiring")
+
+console.log('sizning ismingiz ' + ism)
+console.log('sizning yosh ' + yosh);
+console.log('sizning javob 20+15=' + odam); 
+console.log('sizning javob 6*9=' + keyin);
+console.log('sizning javob 9*10=' + hello);
+console.log('sizning javob 30*5=' + kettik);
+console.log('sizning javob 100-15=' + kim);
+console.log('sizning javob 5*6=' + world);
+console.log('sizning javob 2*2=' + school);
+console.log('sizning javob 30*2=' + index);
